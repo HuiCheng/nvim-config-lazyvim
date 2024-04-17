bc0672e9c82a8894e5102e48083bf532c4e005a9
hui cheng <hui.cheng@naturali.io>
2024-04-17 21:59:20 +0800

    update

--- lua/plugins/vim-translator.lua
+++ lua/plugins/vim-translator.lua
@@ -2,9 +2,9 @@ return {
     {
         "voldikss/vim-translator",
         init = function()
-            vim.keymap.set("n", "<space>tsw", ":TranslateW<CR>", { desc = "Translate curr cursor word" })
-            vim.keymap.set("v", "<space>tsw", ":TranslateW<CR>", { desc = "Translate curr selected words" })
-            vim.keymap.set("n", "<space>tsx", ":TranslateX<CR>", { desc = "Translate the text in the clipboard" })
+            vim.keymap.set("n", "<space>tew", ":TranslateW<CR>", { desc = "Translate curr cursor word" })
+            vim.keymap.set("v", "<space>tew", ":TranslateW<CR>", { desc = "Translate curr selected words" })
+            vim.keymap.set("n", "<space>tex", ":TranslateX<CR>", { desc = "Translate the text in the clipboard" })
         end,
     },
 }
