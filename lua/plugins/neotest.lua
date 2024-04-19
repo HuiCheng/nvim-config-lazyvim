0f4bc2752f1c51986648ecf0581d71b2d2e7e819
hui cheng <hui.cheng@naturali.io>
2024-04-19 23:33:43 +0800

    update: go test -count=1

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -2,5 +2,12 @@ return {
     {
         "nvim-neotest/neotest",
         url = "https://github.com/HuiCheng/neotest.git",
+        opts = {
+            adapters = {
+                ["neotest-go"] = {
+                    args = { "-count=1" },
+                },
+            },
+        },
     },
 }
